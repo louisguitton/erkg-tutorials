5345226552fc2716141af9a3c7a0eda1fe2a4e5b
Louis Guitton <louisguitton93@gmail.com>
2024-08-17 19:16:52 +0200

    refactor scraper

--- src/scraper.py
+++ src/scraper.py
@@ -9,59 +9,68 @@ SCRAPE_HEADERS: dict[str, str] = {
 }
 
 URLS = [
-     "https://www.icij.org/investigations/pandora-papers/secret-real-estate-purchases-are-a-driving-force-behind-the-offshore-economy/",
-     "https://www.icij.org/investigations/pandora-papers/former-czech-leaders-secret-french-estate-revealed-in-pandora-papers-listed-for-sale/"
+    "https://www.icij.org/investigations/pandora-papers/secret-real-estate-purchases-are-a-driving-force-behind-the-offshore-economy/",
+    "https://www.icij.org/investigations/pandora-papers/former-czech-leaders-secret-french-estate-revealed-in-pandora-papers-listed-for-sale/",
 ]
 
 
-def search(tag, attrs):
-    """Parsing only part of the document, using the fact that an ICIJ article has a header and a body.
+class IcijScraper(BeautifulSoup):
 
-    References:
-    - https://stackoverflow.com/a/34536484/3823815
-    - https://www.crummy.com/software/BeautifulSoup/bs4/doc/#parsing-only-part-of-a-document
-    - https://www.crummy.com/software/BeautifulSoup/bs4/doc/#a-function
-    """
-    if tag == "header" and "post-header" in attrs.get("class", []):
-        return True
+    def __init__(self, markup):
+        super().__init__(markup, features="html.parser", parse_only=SoupStrainer(self._get_article))
 
-    if tag == "div" and "post-body" in attrs.get("class", []):
-        return True
-    
-    # TODO: stretch goal: demo advanced search based scraping to filter out news recommendations
-    """   
-    if tag == "div" and any(forbidden_class in attrs.get("class", []) for forbidden_class in ["newsrelated-widget", "banner-donation-default", "responsive-iframe-widget"]):
-        return False
-    """
+    def _get_article(self, tag, attrs):
+        """Parsing only part of the document, using the fact that an ICIJ article has a header and a body.
+
+        References:
+        - https://stackoverflow.com/a/34536484/3823815
+        - https://www.crummy.com/software/BeautifulSoup/bs4/doc/#parsing-only-part-of-a-document
+        - https://www.crummy.com/software/BeautifulSoup/bs4/doc/#a-function
+        """
+        if tag == "header" and "post-header" in attrs.get("class", []):
+            return True
+
+        if tag == "div" and "post-body" in attrs.get("class", []):
+            return True
+
+        # TODO: stretch goal: demo advanced search based scraping to filter out news recommendations
+        """
+        if tag == "div" and any(
+            forbidden_class in attrs.get("class", []) for forbidden_class in [
+                "newsrelated-widget",
+                "banner-donation-default",
+                "responsive-iframe-widget"
+                ]
+            ):
+            return False
+        """
 
 
-scrape_nlp: spacy.Language = spacy.load(SPACY_MODEL)
+if __name__ == "__name__":
+    scrape_nlp: spacy.Language = spacy.load(SPACY_MODEL)
 
-# ref: https://spacy.io/api/docbin
-doc_bin = DocBin()
+    # ref: https://spacy.io/api/docbin
+    doc_bin = DocBin()
 
-for url in URLs:
-
-    response: requests.Response = requests.get(
-        url,
-        headers=SCRAPE_HEADERS,
-    )
-
-    soup: BeautifulSoup = BeautifulSoup(
-        response.text, features="html.parser", parse_only=SoupStrainer(search)
-    )
-    text_contents = soup.find_all(
-        ["h1", "p", "figcaption"],
-    )
-
-    scrape_doc: spacy.tokens.doc.Doc = scrape_nlp(
-        "\n".join(
-            [
-                text_content.text.strip() + "." * (idx == 0)
-                for idx, text_content in enumerate(text_contents)
-            ]
+    for url in URLS:
+        response: requests.Response = requests.get(
+            url,
+            headers=SCRAPE_HEADERS,
         )
-    )
-    doc_bin.add(scrape_doc)
 
-doc_bin.to_disk("./data.spacy")
+        soup: IcijScraper = IcijScraper(response.text)
+        text_contents = soup.find_all(
+            ["h1", "p", "figcaption"],
+        )
+
+        scrape_doc: spacy.tokens.doc.Doc = scrape_nlp(
+            "\n".join(
+                [
+                    text_content.text.strip() + "." * (idx == 0)
+                    for idx, text_content in enumerate(text_contents)
+                ]
+            )
+        )
+        doc_bin.add(scrape_doc)
+
+    doc_bin.to_disk("./dataset.spacy")
