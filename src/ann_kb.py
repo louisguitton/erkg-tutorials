11dfbf7fa304f2fcb565a22ac0ca495f9e47257d
Louis Guitton <louisguitton93@gmail.com>
2024-09-08 18:56:01 +0200

    add clean KB

--- src/ann_kb.py
+++ src/ann_kb.py
@@ -4,43 +4,147 @@ Adapted from:
 - https://github.com/explosion/spaCy/blob/master/spacy/kb/kb_in_memory.pyx
 """
 
-# ref: https://lancedb.github.io/lancedb/embeddings/available_embedding_models/text_embedding_functions/sentence_transformers/
-from typing import Iterable
+from typing import TypedDict
 
 import lancedb
 from lancedb.embeddings import get_registry
 from lancedb.pydantic import LanceModel, Vector
-from spacy.kb import Candidate, InMemoryLookupKB, KnowledgeBase
+from pydantic import create_model
 from spacy.tokens import Span
-from spacy.vocab import Vocab
+
+from ann_linker import Alias, Entity
+
+FAST_AND_SMALL = "sentence-transformers/all-MiniLM-L6-v2"
 
 
-class AnnKnowledgeBase(KnowledgeBase):
+class EntityCandidate(TypedDict):
+    alias: Alias
+    _distance: float
 
-    def __init__(self, vocab: Vocab, entity_vector_length: int):
+
+class AnnKnowledgeBase:
+
+    def __init__(self, uri: str):
         """Create an AnnKnowledgeBase."""
-        super().__init__(vocab, entity_vector_length)
+        # connect to the LanceDB
+        self.db = lancedb.connect(uri)
 
-    ### CORE
-    # add_entity
-    # add_alias
-    def get_candidates(self, mention: Span) -> Iterable[Candidate]:
+        # Embedding model for aliases and entities
+        # any LanceDB-compatible model is available as a drop-in replacement
+        # ref: https://lancedb.github.io/lancedb/embeddings/default_embedding_functions/#text-embedding-functions
+        self.encoder = (
+            get_registry().get("sentence-transformers").create(name=FAST_AND_SMALL, device="cpu")
+        )
+
+        # we need pydantic classes to define the Arrow schemas of LanceDB tables.
+        # those tables will contain the embedding from our encoder to do the ANN search.
+        # because we want to use the local self.encoder, we can't use the traditional pydantic syntax.
+        # instead, we create the pydantic classes dynamically using pydantic.create_model
+        # ref: https://docs.pydantic.dev/latest/api/base_model/#pydantic.create_model
+        self.LanceAlias = create_model(
+            "LanceAlias",
+            __base__=LanceModel,
+            alias=(Alias, ...),
+            vector=(Vector(self.encoder.ndims()), self.encoder.VectorField()),
+        )
+        self.LanceEntity = create_model(
+            "LanceEntity",
+            __base__=LanceModel,
+            entity=(Entity, ...),
+            vector=(Vector(self.encoder.ndims()), self.encoder.VectorField()),
+        )
+
+        # Parameters for the mention candidate generation
+        self.top_k = 10
+        self.max_distance = 0.5
+
+        self._initialize_db()
+
+    def _embed(self, text: str) -> list[int]:
+        return self.encoder.generate_embeddings([text])[0]
+
+    def _initialize_db(self):
+        # TODO: do better, use mode from params: e.g. the table might already exists
+        self.db.create_table("aliases", schema=self.LanceAlias, mode="overwrite")
+        self.db.create_table("entities", schema=self.LanceEntity, mode="overwrite")
+
+    def add_aliases(self, aliases: list[Alias]):
+        """Build the ANN index of aliases in LanceDB."""
+        table = self.db.open_table("aliases")
+        table.add(
+            [self.LanceAlias(alias=alias, vector=self._embed(alias.alias)) for alias in aliases]
+        )
+
+    def get_candidates(self, mention: Span) -> list[Alias]:
         return self.get_alias_candidates(mention.text)
 
-    def get_alias_candidates(self, alias: str) -> Iterable[Candidate]:
-        # vectorize -> search ANN neighbours against aliases index -> build Iterable[Candidate]
-        return []
+    def get_alias_candidates(self, query: str) -> list[Alias]:
+        """Embed a mention query, search ANN neighbours against the aliases index."""
+        table = self.db.open_table("aliases")
+        results = (
+            table.search(self._embed(query))
+            .metric("cosine")
+            .limit(self.top_k)
+            .select(["alias"])
+            .to_list()
+        )
+        filtered_results = [r for r in results if r["_distance"] < self.max_distance]
+        return [Alias(**result["alias"]) for result in filtered_results]
 
-    def get_vector(self, entity: str):
-        pass
+    def get_entity_candidates(self, query: str) -> list[str]:
+        """Get the entity IDs corresponding to a mention."""
+        return list(
+            set(
+                entity_id
+                for alias in self.get_alias_candidates(query)
+                for entity_id in alias.entities
+            )
+        )
 
-    ### NIT
-    # get_alias_strings
-    # get_prior_prob
+    def add_entities(self, entities: list[Entity]):
+        """Build the ANN index of entities in LanceDB."""
+        table = self.db.open_table("entities")
+        table.add(
+            [
+                self.LanceEntity(entity=entity, vector=self._embed(entity.description))
+                for entity in entities
+            ]
+        )
 
-    ### I/O
-    # to_bytes
-    # from_bytes
-    # to_disk
-    # from_disk
-    # from_disk
+    def disambiguate(
+        self, candidate_entities: list[str], doc_embedding: list[int]
+    ) -> tuple[Entity | None, float]:
+        """Disambiguate candidate entities by getting the most similar to the context in the doc."""
+        table = self.db.open_table("entities")
+        entities_results = (
+            # search the entity ANN index by the embedding of the context in the doc
+            table.search(doc_embedding)
+            .metric("cosine")
+            # prefilter for only the candidate entities
+            .where(f"list_has({candidate_entities}, entity.entity_id)", prefilter=True)
+            # get the argmax
+            .limit(1)
+            # serialize
+            .select(["entity"])
+            .to_list()
+        )
+        if not len(entities_results):
+            return None, 1
+        entity = entities_results[0]["entity"]
+        cosine_score = entities_results[0]["_distance"]
+        return Entity(**entity), cosine_score
+
+    """
+    We looked at the spacy abstractions closely:
+        - spacy.kb.KnowledgeBase
+        - spacy.kb.Candidate
+    We decided to implement a class that does not use them.
+    The reason is that we felt we were shoehorning too much of spacy classes.
+
+    Still, the native spacy classes have the following useful methods that
+    were not implemented here:
+        - to_bytes
+        - from_bytes
+        - to_disk
+        - from_disk
+    """
